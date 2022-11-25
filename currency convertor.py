2519afbe020d98e7b033bb14a8ad1ee26b32ec5e
SwetankRaj23 <119068558+SwetankRaj23@users.noreply.github.com>
2022-11-25 22:15:11 +0530

    Add files via upload

---
+++ currency convertor.py
@@ -0,0 +1,60 @@
+# Python project_Currency convertor
+# Indian Rupee to USD
+currency = int(input("enter Currency: "))
+INRTOUSD = currency/81.49
+print("INRTOUSD = ",INRTOUSD)
+
+# Now USD to Indian Rupee
+currency = int(input("enter Currency: "))
+USDTOINR = currency*81.49
+print("USDTOINR= ",USDTOINR)
+
+# Indian Rupee to Canadian dollar
+currecny = int(input("enter Currency: "))
+INRTOCAD = currency/61.143
+print("INRTOCAD = ",INRTOCAD)
+
+#NOW Canadian to Indian Rupee
+currency = int(input("enter Currency: "))
+CADTOINR = currency*61.143
+print("CADTOINR = ",CADTOINR)
+
+# Indian Rupee to Russian ruble
+currency = int(input("enter Currency: "))
+INRTORUB = currency/1.3472
+print("INRTORUB = ",INRTORUB)
+
+# Now Russian ruble to Indian Rupee
+currecny = int(input("enter Currency: "))
+RUBTOINR = currency*1.3472
+print("RUBTOINR= ",RUBTOINR)
+
+# Indian Rupee to Chinese yuan 
+currency = int(input("enter currecny: "))
+INRTOCNY = currency/11.3873
+print("INRTOCNY = ",INRTOCNY)
+
+# Now Chinese yuan to Indian Rupee
+currency = int(input("enter Currency: "))
+CNYTOINR = currency*11.3873
+print("CNYTOINR = ",CNYTOINR)
+
+# INdian Rupee to Pound
+currency = int(input("enter Currency: "))
+INRTOGBP = currency/98.66
+print("INRTOGBP = ",INRTOGBP)
+
+# Now Pound to Indian Rupee
+currency = int(input("enter Currency: "))
+GBPTOINR = currency*98.66
+print("GBPTOINR = ",GBPTOINR)
+
+# Indiann Rupee to EURO
+currency = int(input("enter Currency: "))
+INRTOEUR = currency/84.95
+print("INRTOEUR = ",INRTOEUR)
+
+# Now EURO to  Indian Rupee
+currency = int(input("enter Currency: "))
+EURTOINR = currency*84.95
+print("EURTOINR = ",EURTOINR)
